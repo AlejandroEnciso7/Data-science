cf0a024e15fe9285000f1adc1eb18c7ad2de0e2e
AlejandroEnciso7 <133036654+AlejandroEnciso7@users.noreply.github.com>
2025-04-15 17:30:04 -0700

    Added functions to calculate store /price values

--- python_data_science1/data_science_functions.py
+++ python_data_science1/data_science_functions.py
@@ -1,5 +1,9 @@
 import math
 import random
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
 def nota_admision():    
     nota_admision = float(input("introduce el año de admision: "))
     print(f' la notade admision fue {nota_admision}') #esto imprime la variable entre llaves, importante poner la "f" al comienzo
@@ -155,26 +159,7 @@ def tabla_multiplicar():
     for i in range(1, 11):
         print(f'{num} x {i} = {num * i}')
 
-""" 4 - Desarrolla un programa que lea un conjunto indefinido de temperaturas en grados Celsius y calcule su promedio. 
-La lectura debe detenerse al ingresar el valor -273°C.
 
-5 - Escribe un programa que calcule el factorial de un número entero proporcionado por el usuario. 
-Recuerda que el factorial de un número entero es el producto de ese número por todos sus antecesores hasta llegar al número 1. 
-Por ejemplo, el factorial de 5 es 5 x 4 x 3 x 2 x 1 = 120.
-
-7 - Los números primos tienen diversas aplicaciones en Ciencia de Datos, como en criptografía y seguridad. 
-Un número primo es aquel que es divisible solo por sí mismo y por 1. Por lo tanto, crea un programa que solicite un número entero y determine si es un número primo o no.
-
-8 - Vamos a comprender la distribución de edades de los pensionistas de una empresa de seguros. 
-Escribe un programa que lea las edades de una cantidad no informada de clientes y muestre la distribución en los intervalos [0-25], [26-50], [51-75] y [76-100]. 
-La entrada de datos se detendrá al ingresar un número negativo.
-
-9 - En una elección para la gerencia de una empresa con 20 empleados, hay cuatro candidatos.
- Escribe un programa que calcule al ganador de la elección. La votación se realizó de la siguiente manera:
-Cada empleado votó por uno de los cuatro candidatos (representados por los números 1, 2, 3 y 4).
-También se contaron los votos nulos (representados por el número 5) y los votos en blanco (representados por el número 6).
-Al final de la votación, el programa debe mostrar el número total de votos para cada candidato, los votos nulos y los votos en blanco.
- Además, debe calcular y mostrar el porcentaje de votos nulos con respecto al total de votos y el porcentaje de votos en blanco con respecto al total de votos."""
 
 lista = [1, 2 ,3 ,4 ,5]
 
@@ -349,3 +334,47 @@ def fecha_valida():
         # Si el mes no está entre 1 y 12
     else:
         print('Fecha invalida')
+
+def grafico_barras():
+    plt.bar(['A', 'B', 'C'], [10, 20, 15]) #.bar toma como parametros el eje x y el eje y, en este caso las letras son el eje "x" y los numeros son el eje "y"
+    plt.title('Gráfico de Barras')
+
+def grafico_estudiantes():
+    estudiantes = ['Juan', 'María', 'Pedro']
+    estudiante = random.choice(estudiantes) #esto elige un elemento aleatorio de la lista estudiantes
+
+def random_number():
+    lista = [8, 12, 54, 23, 43, 1, 90, 87, 105, 77]
+    print(random.choice(lista) )
+
+def sorteo_participantes():
+    cantidad_participantes = int(input("Ingrese la cantidad de participantes: "))
+    numero_sorteado = random.randint(1, cantidad_participantes)
+    print(f"El número sorteado es: {numero_sorteado}")
+
+def token_generator():
+    token = 9998
+    token_generado = random.randint(1000, token)
+    nombre_usuario= str(input(' escribe tu nombre: '))
+    print(f' hola {nombre_usuario}, tu token es {token_generado}')
+
+def ensalada_random():
+    frutas = ["manzana", "banana", "uva", "pera", "mango", "coco", "sandia", "fresa", "naranja", "maracuya", "kiwi", "cereza"]
+    ensalada = random.sample(frutas, 3) #esto elige 3 elementos aleatorios de la lista frutas y los coloca en una nueva lista llamada ensalada
+    print(f'ensalada de frutas: {ensalada}')
+def raiz_entera():
+    numero = [2,8,15,23,91,112,256]
+    raices_enteras = [num for num in numero if math.sqrt(num).is_integer()] #esto crea una nueva lista con los numeros que son raices enteras, no decimales
+    print('numeros con raices enteras:', raices_enteras)
+
+def raiz():
+    numeros = [2, 8, 15, 23, 91, 112, 256]
+    raices_enteras = [num for num in numeros if math.sqrt(num) % 1 == 0]
+    print("Números con raíces enteras:", raices_enteras)
+
+def jardin():
+    precio = 25.00
+    cantidad = float(input(' cuanto cesped necesitas?'))
+    area = math.pi * math.pow(cantidad, 2) #esto calcula el area de un circulo, el area es igual a pi por el radio al cuadrado
+    total = precio * area
+    print(f'el precio total es {total}')
--- python_data_science1/data_science_main.py
+++ python_data_science1/data_science_main.py
@@ -1,10 +1,17 @@
 import data_science_functions as lib
 import math
 import random
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+matplotlib.__version__
+
+
 # Este archivo contiene funciones que se pueden usar en el archivo data_science_py.py
 #VARIABLES
 lista = [1, 2 ,3 ,4 ,5]
 
 
 # lib.num_in_list(lista)
-lib.lista_enteros_random()
+lib.jardin()
