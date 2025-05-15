94d03575155855d22d70e4a5195222b25247d1c2
AlejandroEnciso7 <133036654+AlejandroEnciso7@users.noreply.github.com>
2025-05-14 18:38:00 -0700

    added function to pivot and create a matrix

--- python_data_science_2/numpy_panda_functions.py
+++ python_data_science_2/numpy_panda_functions.py
@@ -12,3 +12,19 @@ def lista_a_array(lista):
     print("Array: ", array)
     
     return np.array(lista)
+
+#se pueden convertir filas en columnas y columnas en filas (haciendo un pivot) con la funcoin transpose
+def pivotear_array(array):
+    # crea un array Numpy
+    array = np.array([[1, 2, 3], [4, 5, 6]])
+
+    # pivotea el array
+    array_pivot = array.T
+
+    print("Array original:\n", array)
+    print("Array pivoteado:\n", array_pivot)
+    
+    return array.T
+
+# Shape(archivodatos) devuelve la forma de un array o dataframe y se puede utilizar para conocer la dimensiones de ua matriz
+
--- python_data_science_2/numpy_pandas_main.py
+++ python_data_science_2/numpy_pandas_main.py
@@ -1,5 +1,26 @@
 import numpy_panda_functions as lib
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
+url = 'https://gist.githubusercontent.com/ahcamachod/9be09de793dc3bf1e6c3d98eb4e5b1ef/raw/21b85572693200040e11284ef6dcfc3457ec8e11/citrus.csv'
+datos = np.loadtxt(url, delimiter=',', usecols=np.arange(1, 6, 1), skiprows=1)
 
 #una lista se puede convertir en un array de numpy lo que permite trabajar con matrices y operaciones matematicas
 
+fechas = lib.pivotear_array()[:,0]
+#esto toma todas las filas  la primera columna del array y la convierte en una lista
+precios = lib.pivotear_array()[:,1:]
+#esto toma todas las filas y de la primera columna en adelante y la convierte en una lista
+
+plt.plot(fechas, precios[:,0])
+# hace un grafico con todas las fechas y el precio de la primera columna
+moscu = precios[:.0]
+kalinigrado = precios[:.1]
+petersburgo = precios[:.2]
+krasnodar = precios[:.3]
+ekaterinburgo = precios[:.4]
+plt.plot(fechas, moscu)
+
+moscu_1= moscu[0:12] #aqui se comparan los primeros doce meses 
+moscu_2= moscu[12:24]
