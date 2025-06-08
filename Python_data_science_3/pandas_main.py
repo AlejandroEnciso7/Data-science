c88b9e7a12a77176ef68fadc3c3968b5d6c96421
AlejandroEnciso7 <133036654+AlejandroEnciso7@users.noreply.github.com>
2025-06-08 14:56:35 -0700

    added json and other format methods

--- Python_data_science_3/pandas_main.py
+++ Python_data_science_3/pandas_main.py
@@ -18,10 +18,50 @@ datos = pd.read_csv(url)
 # pd.read_csv('datos.csv', usecols = ['Tipo', 'Valor', 'Condominio']) # si solo quiero leer ciertas columnas del archivo
 # tambein se pueden usar los indices de las columnas con usecols=[0,1,4] # si quiero leer las columnas 0, 1 y 4 del archivo
 # pd.read_csv()
+
 # pd.read_excel()
-# pd.read_json()
+#archivo = 'nombre_archivo.xlsx'
+# pd.read_excel(archivo)
+#pd.ExcelFile(archivo).sheet_names # me muestra el nombre de las hojas del archivo excel
+# pd.read_excel(archivo, sheet_name='Hoja1') # me permite leer una hoja especifica del archivo excel
+# pd.read_excel(archivo), sheet_name'hoja1', usecols = 'A:C') # me permite leer un rango de columnas especificas de la hoja del archivo excel que le solicite
+
+# LEER ARCHIVOS DE EXCEL ONLINE
+# pd.read_excel('https://url_del_archivo.xlsx')
+# es recomendable quital la parte del edit y utilizar al final '/gviz/tq' esto indica que es una hoja de excel de google y permite visualizar el archivo en google colab
+# tambien tener enceunta el ID del archivo, ¿Y dónde encontramos ese id? En el propio link. Es todo lo que aparece entre d/ y /edit.
+# Una vez hecho esto, podemos leer los datos pasando la variable url a la función read_csv:, datos = pd.read_csv(url)
+# Para guardar los datos en formato CSV, basta con utilizar la función to_csv y, entre paréntesis, darle un nombre al archivo agregando el parámetro index=False para que no se agregue una nueva columna de índice:
+#datos.to_csv('datos_emisiones_co2.csv', index=False)
+
+# pd.read_json('nombre_archivo.json')
+# df_normalizado = pd.json_normalize(nombre_archivo.json ['nombre_columna_donde_hay_anidacion']) # esto permite normalizar un archivo json y convertirlo en un dataframe de pandas,
+# para cuadno el archivo tiene una estructura anidada dentro de si misma 
+
+# Una forma común de acceder a las API es a través de la biblioteca requests. Para eso, también es necesario importar el módulo json:
+# import requests
+# import json
+
+# se utiliza requests.get para obtener los datos de la API
+# datos_frutas = requests.get('https://fruityvice.com/api/fruit/all')
+# para visualizar los datos obtenidos de la API se utiliza json.loads(archivo/variable.text)
+# resultado = json.loads(datos_frutas.text)
+#para finalmente visualirzar un dataframe:
+# pd.DataFrame(resultado)
+
 # pd.read_html()
+# Para leer la página, simplemente pase la dirección a la función read_html. Para que se muestre la primera tabla asignamos el primer elemento de la lista, con [0]:
+# pd.read_html('https://es.wikipedia.org/wiki/Anexo:Pa%C3%ADses_y_territorios_dependientes_por_poblaci%C3%B3n')[0] (esto es para cuadno la pagina tiene varias tablas)
+
 # pd.read_sql()
+# import sqlalchemy
+# from sqlalchemy import create_engine, MetaData, Table, inspect, text
+
+#los parametors de skiprows= x # permite saltar las primeras x filas del archivo
+# Skipfooter= x # permite saltar las ultimas x filas del archivo
+
+# IMPORTAR/GUARDAR ARCHIVOS
+#datos_seleccion.to_csv('nombre_arhchivo.csv', index=False)
 
 # explore the dataset
 datos.shape
